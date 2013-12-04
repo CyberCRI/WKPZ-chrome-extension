7c46697ba263675d6c7abc45aca1ffa6b5b03a40
duong tam kien <tk@deveha.com>
2013-12-04 11:30:24 +0100

    push raw informations to the api

--- background.js
+++ background.js
@@ -1,6 +1,12 @@
 chrome.webNavigation.onCommitted.addListener(function (e){
 
   console.log("coucou");
+  console.log(e)
+
+  e.time = e.timeStamp;
+
+
+  $.post("http://api.wkpdz.11d.im/steps", e);
 
 }, { url : [
   { hostSuffix : "wikipedia.org" }
